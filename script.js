206e6745c4970bce966bfb6bb8fbf270a9251606
Trupti100 <127935389+Trupti100@users.noreply.github.com>
2023-09-08 09:49:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const panels = document.querySelectorAll('.panel');
+
+function toggleActive() {
+  panels.forEach(panel => panel.classList.remove('active'));
+  this.classList.add('active');
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleActive));
